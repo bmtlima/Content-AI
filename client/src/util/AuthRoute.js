5e7686b3d0f287e3309d236fb71534611bfe1f8c
Bruno Makoto Tanabe de Lima <bruno.makoto04@gmail.com>
2023-06-27 02:18:45 -0300

    working

---
+++ client/src/util/AuthRoute.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { Route, Navigate } from 'react-router-dom';
+
+import { AuthContext } from '../context/auth';
+
+function AuthRoute({ component: Component, ...rest }) {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Route
+      {...rest}
+      element={user ? <Navigate to="/" /> : <Component />}
+    />
+  );
+}
+
+export default AuthRoute;
+
+
+/*import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+
+import { AuthContext } from '../context/auth';
+
+function AuthRoute({ component: Component, ...rest }) {
+  const { user } = useContext(AuthContext);
+
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        user ? <Redirect to="/" /> : <Component {...props} />
+      }
+    />
+  );
+}
+
+export default AuthRoute;*/
